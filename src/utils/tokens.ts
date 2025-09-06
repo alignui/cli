511b82bdf55ebc0143f6b66a06848902c78a3ee1
Tolunay Dönmez <f.tolunaydonmez@gmail.com>
2025-09-06 21:33:49 +0300

    refactor(v0.6): general-fix

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -757,29 +757,29 @@ export const shadows = {
   'regular-sm': '0 2px 4px #1b1c1d0a',
   'regular-md': '0 16px 32px -12px #0e121b1a',
   'button-primary-focus': [
-    '0 0 0 2px theme(colors.bg[white-0])',
-    '0 0 0 4px theme(colors.primary[alpha-10])',
+    '0 0 0 2px var(--color-bg-white-0)',
+    '0 0 0 4px var(--color-primary-alpha-10)',
   ],
   'button-important-focus': [
-    '0 0 0 2px theme(colors.bg[white-0])',
-    '0 0 0 4px theme(colors.neutral[alpha-16])',
+    '0 0 0 2px var(--color-bg-white-0)',
+    '0 0 0 4px var(--color-neutral-alpha-16)',
   ],
   'button-error-focus': [
-    '0 0 0 2px theme(colors.bg[white-0])',
-    '0 0 0 4px theme(colors.red[alpha-10])',
+    '0 0 0 2px var(--color-bg-white-0)',
+    '0 0 0 4px var(--color-red-alpha-10)',
   ],
   'fancy-buttons-neutral': ['0 1px 2px 0 #1b1c1d7a', '0 0 0 1px #242628'],
   'fancy-buttons-primary': [
     '0 1px 2px 0 #0e121b3d',
-    '0 0 0 1px theme(colors.primary[base])',
+    '0 0 0 1px var(--color-primary-base)',
   ],
   'fancy-buttons-error': [
     '0 1px 2px 0 #0e121b3d',
-    '0 0 0 1px theme(colors.error[base])',
+    '0 0 0 1px var(--color-error-base)',
   ],
   'fancy-buttons-stroke': [
     '0 1px 3px 0 #0e121b1f',
-    '0 0 0 1px theme(colors.stroke[soft-200])',
+    '0 0 0 1px var(--color-stroke-soft-200)',
   ],
   'toggle-switch': ['0 6px 10px 0 #0e121b0f', '0 2px 4px 0 #0e121b08'],
   'switch-thumb': ['0 4px 8px 0 #1b1c1d0f', '0 2px 4px 0 #0e121b14'],
